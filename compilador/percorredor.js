5056097238e4a3ba63bdb2c1a3c633f4585a48c1
Elender <elenderg@gmail.com>
2022-03-26 18:21:37 -0300

    Add files via upload

--- compilador/percorredor.js
+++ compilador/percorredor.js
@@ -11,22 +11,27 @@ function PlanificaArray(ArrayComSubarrays) {
     return ArrayPlanificado;
 }
 
-function DivideStringComSimbolos(ArrayDeTermos,Simbolo) {    
-    let NovoArrayDeTermos;
+function DivideStringComSimbolos(ArrayDeTermos,Simbolo) {
+    let NovoArrayDeTermos;    
     let SimboloComEspacos = " " + Simbolo + " ";
     if(Simbolo != undefined){
         for (let indice = 0; indice < ArrayDeTermos.length; indice++) {            
-            if(ArrayDeTermos[indice].includes(Simbolo)){                
-                ArrayDeTermos[indice] = ArrayDeTermos[indice].replace(Simbolo,SimboloComEspacos); 
+            if(ArrayDeTermos[indice].includes(Simbolo) // verifica se a string contém o símbolo
+            && ArrayDeTermos[indice].length > 1
+            && ArrayDeTermos[indice] !== SimboloComEspacos) // verifica se a string não é o próprio símbolo em si
+            {                
+                ArrayDeTermos[indice] = ArrayDeTermos[indice].replaceAll(Simbolo,SimboloComEspacos);                
                 // insere um espaço entre o símbolo e a string
                 ArrayDeTermos[indice] = ArrayDeTermos[indice].split(" ");
+                // cria um novo array dentro do array original, com os símbolos em indices distintos
                 NovoArrayDeTermos = ArrayDeTermos.flat(Infinity);                
-                //console.log(NovoArrayDeTermos);                
+                //cria um novo array com o array original planificado       
             }                    
         }
-        console.log(Simbolo + " devidamente separado.");
-        return NovoArrayDeTermos; 
+        
     }
+    console.log(Simbolo + " devidamente separado.");
+    return NovoArrayDeTermos; 
 }
 
 function DeslocaElementosDoArray(ArrayOriginal, IndiceInicial, IndiceFinal) {
@@ -46,16 +51,21 @@ function ExaminaTermo(codigo) {
         ,"^","$","€","£","¥","¢","¤","¬","¦","ª","º"
         ,"¹","²","³","¼","½","¾",'"'
     ]
-    let ArrayDeTermos
+    let ArrayDeTermos;
     for (let index = 0; index < Simbolos.length; index++) {
         ArrayDeTermos = DivideStringComSimbolos(codigo,Simbolos[index]); // divide string with symbols
     }
-    ArrayDeTermos = ArrayDeTermos.filter((elemento) => elemento);
+    let Novo;
+    for (let index = 0; index < Simbolos.length; index++) {
+        Novo = DivideStringComSimbolos(ArrayDeTermos,Simbolos[index]); // divide string with symbols
+    }    
+    
+    Novo = Novo.filter((elemento) => elemento);
     //ArrayDeTermos = ArrayDeTermos.join('');
-    console.log(ArrayDeTermos);
-    for (let IndiceDoTermoAtual = 0; IndiceDoTermoAtual < ArrayDeTermos.length; IndiceDoTermoAtual++) {
+    console.log(Novo);
+    for (let IndiceDoTermoAtual = 0; IndiceDoTermoAtual < Novo.length; IndiceDoTermoAtual++) {
         //console.log(IndiceDoTermoAtual);
-        const TermoAtual = ArrayDeTermos[IndiceDoTermoAtual];
+        const TermoAtual = Novo[IndiceDoTermoAtual];
         //console.log(TermoAtual);
         if (TermosValidos.includes(TermoAtual)) {
             console.log(IndiceDoTermoAtual + ". Encontrado termo válido: " + TermoAtual);
