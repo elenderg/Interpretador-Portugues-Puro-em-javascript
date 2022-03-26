e1421fa9f7e299b36694c13d038ff88fde15166c
Elender <elenderg@gmail.com>
2022-03-26 17:08:39 -0300

    Add files via upload

--- compilador/percorredor.js
+++ compilador/percorredor.js
@@ -1,10 +1,7 @@
 function DivideCodigo() {
-    let codigo = document.getElementById('codigo').value;
-    //console.log(codigo);
-    codigo = codigo.toLowerCase();
-    //console.log(codigo);
-    codigo = codigo.split(" ");
-    //console.log(codigo);
+    let codigo = document.getElementById('codigo').value;    
+    codigo = codigo.toLowerCase();    
+    codigo = codigo.split(" ");    
     ExaminaTermo(codigo);
 }
 
@@ -14,91 +11,58 @@ function PlanificaArray(ArrayComSubarrays) {
     return ArrayPlanificado;
 }
 
-function DivideStringComSimbolos(ArrayDeTermos,Simbolos) {
-    //console.log(ArrayDeTermos);
-    //const VerificaElentosIguais = ArrayDeTermos.some((Termo) => Simbolos.includes(Termo)); // checks if any element of ArrayDeTermos is in Simbolos
-    // if a string has any symbol on it, it will be split into substrings
-    /*if (VerificaElentosIguais) {
-        console.log("Tem simbolos");
-        let ArrayDeTermosComSimbolos = Simbolos;
-        ArrayDeTermosComSimbolos.forEach((Simbolo) => {
-            ArrayDeTermos.forEach((Termo) => {
-                if (Termo.includes(Simbolo)) {
-                    let ArrayDeSubtermos = Termo.split(Simbolo);
-                    ArrayDeTermos.splice(ArrayDeTermos.indexOf(Termo), 1, ...ArrayDeSubtermos);
-                }
-            }
-            );*/
-
-    for (let SimboloAtual = 0; SimboloAtual < Simbolos.length; SimboloAtual++) {
-        const Simbolo = Simbolos[SimboloAtual];
-
-        for (let i = 0; i < ArrayDeTermos.length; i++) {            
-            if (ArrayDeTermos[i].includes(Simbolo)) {
-                ArrayDeTermos[i] = ArrayDeTermos[i].substr(0, ArrayDeTermos.length + 1);
-                //console.log(ArrayDeTermos[i]);
-                let IndiceDoProximoTermo = ArrayDeTermos.length + 1;
-                //checks if a index in a array is null
-                if (ArrayDeTermos[IndiceDoProximoTermo] === undefined) {
-                    ArrayDeTermos[IndiceDoProximoTermo] = Simbolo;
-                    return ArrayDeTermos;
-                }
-                //console.log(IndiceDoProximoTermo);
-                //console.log(ArrayDeTermos);
-                ArrayDeTermos.splice(IndiceDoProximoTermo, 0, Simbolo);
-                //console.log(ArrayDeTermos);
-            }    
+function DivideStringComSimbolos(ArrayDeTermos,Simbolo) {    
+    let NovoArrayDeTermos;
+    let SimboloComEspacos = " " + Simbolo + " ";
+    if(Simbolo != undefined){
+        for (let indice = 0; indice < ArrayDeTermos.length; indice++) {            
+            if(ArrayDeTermos[indice].includes(Simbolo)){                
+                ArrayDeTermos[indice] = ArrayDeTermos[indice].replace(Simbolo,SimboloComEspacos); 
+                // insere um espaço entre o símbolo e a string
+                ArrayDeTermos[indice] = ArrayDeTermos[indice].split(" ");
+                NovoArrayDeTermos = ArrayDeTermos.flat(Infinity);                
+                //console.log(NovoArrayDeTermos);                
+            }                    
         }
-        
+        console.log(Simbolo + " devidamente separado.");
+        return NovoArrayDeTermos; 
     }
-
-    /*for (let i = 0; i < ArrayDeTermos.length; i++) {
-        if (ArrayDeTermos[i].includes(Simbolo)) {
-            ArrayDeTermos[i] = ArrayDeTermos[i].substr(0, ArrayDeTermos.length + 1);
-            console.log(ArrayDeTermos[i]);
-            let IndiceDoProximoTermo = ArrayDeTermos.length + 1;
-            //checks if a index in a array is null
-            if (ArrayDeTermos[IndiceDoProximoTermo] === undefined) {
-                ArrayDeTermos[IndiceDoProximoTermo] = Simbolo;
-                return ArrayDeTermos;
-            }
-            console.log(IndiceDoProximoTermo);
-            console.log(ArrayDeTermos);
-            ArrayDeTermos.splice(IndiceDoProximoTermo, 0, Simbolo);
-            console.log(ArrayDeTermos);
-        }
-    }
-    console.log(ArrayDeTermos);*/
-    console.log("Simbolos devidamente separados.");
-    return ArrayDeTermos;
 }
 
+function DeslocaElementosDoArray(ArrayOriginal, IndiceInicial, IndiceFinal) {
+    var elemento = ArrayOriginal[IndiceInicial];
+    ArrayOriginal.splice(IndiceInicial, 1);
+    ArrayOriginal.splice(IndiceFinal, 0, elemento);
+}
 
 
 function ExaminaTermo(codigo) {
     // flatten array and its subarrays 
     let TermosValidos = PlanificaArray(PalavrasChave);
-    console.log("A função PlanificaArray foi finalizada");
-    console.log(codigo);
+    console.log("A função PlanificaArray foi finalizada");    
     let Simbolos = [
         ".",",",":",";","[","]","(",")","º","ª","°","+"
         ,"-","%","/","*","=",">","<","¬","|","&","#","~"
         ,"^","$","€","£","¥","¢","¤","¬","¦","ª","º"
-        ,"¹","²","³","¼","½","¾",
+        ,"¹","²","³","¼","½","¾",'"'
     ]
-    let ArrayDeTermos = DivideStringComSimbolos(codigo,Simbolos); // divide string with symbols
-    console.log(ArrayDeTermos);    
-    console.log("A função 'DivideStringComSimbolo' foi finalizada");        
+    let ArrayDeTermos
+    for (let index = 0; index < Simbolos.length; index++) {
+        ArrayDeTermos = DivideStringComSimbolos(codigo,Simbolos[index]); // divide string with symbols
+    }
+    ArrayDeTermos = ArrayDeTermos.filter((elemento) => elemento);
+    //ArrayDeTermos = ArrayDeTermos.join('');
+    console.log(ArrayDeTermos);
     for (let IndiceDoTermoAtual = 0; IndiceDoTermoAtual < ArrayDeTermos.length; IndiceDoTermoAtual++) {
-        console.log(IndiceDoTermoAtual);
+        //console.log(IndiceDoTermoAtual);
         const TermoAtual = ArrayDeTermos[IndiceDoTermoAtual];
         //console.log(TermoAtual);
         if (TermosValidos.includes(TermoAtual)) {
-            console.log("Encontrado termo válido:" + TermoAtual);
+            console.log(IndiceDoTermoAtual + ". Encontrado termo válido: " + TermoAtual);
             CategorizaTermo(TermoAtual);
         }
         if (!TermosValidos.includes(TermoAtual)) {
-            console.log("O termo:", TermoAtual, "é inválido.")
+            console.log(IndiceDoTermoAtual + ". Encontrado termo inválido: " + TermoAtual);
         }
     }
 }
